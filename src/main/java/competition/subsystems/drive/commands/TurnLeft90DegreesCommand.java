6fa31b4f21b8b97b0ffecd5fe44134c6704498be
Clulogh <197954988+Clulogh@users.noreply.github.com>
2025-11-15 14:32:33 -0800

    Updated turn left to 90, successfully worked

--- src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -6,6 +6,7 @@ import xbot.common.command.BaseCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
 public class TurnLeft90DegreesCommand extends BaseCommand {
 
@@ -13,42 +14,33 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
     PoseSubsystem pose;
 
     double target;
-    //double error;
     double previousPos = 0;
-    double speeD;
 
     @Inject
     public TurnLeft90DegreesCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
         this.drive = driveSubsystem;
         this.pose = pose;
 
+
     }
 
     @Override
     public void initialize() {
         target = pose.getCurrentHeading().getDegrees() + 90; //90 is the target not the error
-        //you need to reduce the +90, so it goes down as it gets closer to target. If it keeps +90 then it will pass the target
-
+        // 150+90=240 .. 240-360=-120 might have to minus 360 to target
+        if (target > 180) { //target is out of bounds so bigger than 180.
+            target = target - 360; //since its out of bounds and bigger than 180, you have to subtract it by 360
+        }
 
     }
-    //circle is 360, 90 is one quarter of the circle, get degree is turning in place so no moving.
 
     @Override
     public void execute() {
-        //turn 90 degrees instantly
-        double power = pose.getCurrentHeading().getDegrees();
-        double error = pose.getCurrentHeading().getDegrees() - previousPos;
-
-        if (error < .001) {
-            drive.tankDrive(1, speeD);
-
+        //turn 90 degrees
         double speed = pose.getCurrentHeading().getDegrees() - previousPos;
-        double power = pose.getCurrentHeading().getDegrees() * error - previousPos * speed;
+        double power = target - speed;
+        double error = target - pose.getCurrentHeading().getDegrees();
+        // higher distance error higher power,more power less speed ||| power = error - speed
+        drive.tankDrive(-power, power);
 
-
-
-
-    }
-
-
-}
+    }}
